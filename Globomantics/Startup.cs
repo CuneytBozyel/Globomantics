ea7564032ab26fb72b16eaaabecbad6cd29bc797
Cuneyt B <cuneytbozyel@gmail.com>
2019-01-21 00:54:43 +0300

    Add services, and ViewModel Layer project

--- Globomantics/Startup.cs
+++ Globomantics/Startup.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using WebApp.Services;
 
 namespace Globomantics
 {
@@ -15,6 +14,13 @@ namespace Globomantics
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddSingleton<IConferenceService, ConferenceMemoryService>();
+            services.AddSingleton<IProposalService, ProposalMemoryService>();
+            services.AddSingleton(x =>
+                new HttpClient { BaseAddress = new Uri("http://localhost:44311") });
+
+           // services.Configure<GlobomanticsOptions>(configuration.GetSection("Globomantics"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
