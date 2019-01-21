96ebef6b3a22cf7a45c90ca8a8d58824e5b0b48b
Cuneyt B <cuneytbozyel@gmail.com>
2019-01-21 21:41:45 +0300

    Add Controller and Views

--- Globomantics/Startup.cs
+++ Globomantics/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Globalization;
 using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using WebApp.Services;
@@ -11,6 +13,12 @@ namespace Globomantics
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -21,21 +29,22 @@ namespace Globomantics
             services.AddSingleton(x =>
                 new HttpClient { BaseAddress = new Uri("http://localhost:44311") });
 
-           // services.Configure<GlobomanticsOptions>(configuration.GetSection("Globomantics"));
+           services.Configure<GlobomanticsOptions>(configuration.GetSection("Globomantics"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Conference}/{action=Index}/{id?}");
             });
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
         }
     }
 }
